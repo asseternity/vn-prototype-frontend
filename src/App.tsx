abb8ecf5f4ce716ab35eb32a9be61ed8e6650186
asseternity <asset_n@proton.me>
2025-11-17 12:36:44 +0400

    feat: init login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,119 +1,20 @@
 // dependencies
-import { useEffect, useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 // components
-import Background from './components/background';
-import DialogueBox from './components/dialogue_box';
-import Portrait from './components/portrait';
-import { Character } from './lib/vn_objects';
-
-// assets
-import bg_test from '/bg_test.jpg';
-import { script } from './lib/vn_script';
-
-// types
-import type { VNLine } from './lib/vn_objects';
-type TestResponse = {
-  message: string;
-};
-
-const firstLine = script.flipLine();
+import Login from './components/login';
+import Game from './components/game';
 
 function App() {
-  const [data, setData] = useState<TestResponse | null>(null);
-  const [currentLine, setCurrentLine] = useState<VNLine | null>(firstLine);
-  const [visiblePortraits, setVisiblePortraits] = useState<Character[]>([]);
-
-  useEffect(() => {
-    // will fetch the story from server in the future
-    const fetchTest = async () => {
-      try {
-        const res = await fetch('http://localhost:3000/test');
-        if (!res.ok) throw new Error('backend refuses to cooperate');
-        const json = await res.json();
-        setData(json);
-      } catch (err) {
-        console.error('fetch is having a meltdown:', err);
-      }
-    };
-
-    fetchTest();
-  }, []);
-
-  const advance = () => {
-    const next = script.flipLine();
-    if (!next) return;
-
-    setCurrentLine(next);
-
-    setVisiblePortraits((prev) => {
-      // Reset all
-      const updated = prev.map((c) => {
-        c.current_speaker = false;
-        return c;
-      });
-
-      // Quit if narration
-      if (!next.speaker) return prev;
-
-      // Ensure speaker is visible
-      let nextList = [...updated];
-      if (!updated.some((c) => c.id === next.speaker!.id)) {
-        if (updated.length < 2) {
-          nextList = [...updated, next.speaker!];
-        } else {
-          const [c1, c2] = updated;
-          const c1Last = c1.lastSpokeIndex(script);
-          const c2Last = c2.lastSpokeIndex(script);
-          const replace = c1Last < c2Last ? c1 : c2;
-          nextList = updated.map((c) =>
-            c.id === replace.id ? next.speaker! : c
-          );
-        }
-      }
-
-      // NOW set the active one
-      next.speaker!.current_speaker = true;
-      return nextList;
-    });
-  };
+  const [username, setUsername] = useState<string>('');
 
   return (
-    <div className="h-full w-full">
-      <h1 className="absolute z-10 text-white p-5">
-        Backend says: {data?.message || 'nothing yet'}
-      </h1>
-      <div className="relative w-full h-full">
-        <Background imgPath={bg_test} />
-        <AnimatePresence>
-          {visiblePortraits.map((char, i) => (
-            <Portrait
-              key={char.id}
-              spritePath={char.portrait}
-              className={
-                i === 0
-                  ? `
-      bottom-0
-      left-1/2 -translate-x-1/2
-      -ml-[20vw] sm:-ml-[15vw] md:-ml-[12vw] lg:-ml-[10vw]
-    `
-                  : `
-      bottom-0
-      left-1/2 -translate-x-1/2
-      ml-[20vw] sm:ml-[15vw] md:ml-[12vw] lg:ml-[10vw]
-    `
-              }
-              active={char.current_speaker}
-            />
-          ))}
-        </AnimatePresence>
-        <DialogueBox
-          name={currentLine?.speaker?.name || ''}
-          text={currentLine?.text || '...'}
-          onContinue={advance}
-        />
-      </div>
+    <div className="h-full w-full flex justify-center items-center">
+      {username === '' ? (
+        <Login usernameCallback={setUsername} />
+      ) : (
+        <Game username={username} />
+      )}
     </div>
   );
 }
---
+++ src/components/login.tsx
@@ -0,0 +1,66 @@
+// dependencies
+import { useState } from 'react';
+
+// types
+type LoginProps = {
+  usernameCallback: Function;
+};
+
+function Login({ usernameCallback }: LoginProps) {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {
+    setPassword('');
+    setUsername('');
+    usernameCallback(username);
+  };
+
+  return (
+    <div className="w-70 flex flex-col gap-5 rounded-xl border-2 border-white p-5 bg-white">
+      <h1 className="text-2xl font-bold text-gray-800 text-center">
+        Please log in
+      </h1>
+      <form onSubmit={handleLogin} className="flex flex-col gap-5">
+        <label htmlFor="email" className="text-gray-700 font-medium">
+          Username
+        </label>
+        <input
+          value={username}
+          type="text"
+          id="username"
+          name="username"
+          placeholder="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          className="border-2 rounded-lg p-3 focus:ring-2 focus:ring-blue-400 focus:outline-none"
+          required
+        />
+        <label htmlFor="password" className="text-gray-700 font-medium">
+          Password
+        </label>
+        <input
+          value={password}
+          type="password"
+          id="password"
+          name="password"
+          placeholder="********"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="border-2 rounded-lg p-3 focus:ring-2 focus:ring-blue-400 focus:outline-none"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-lg shadow-lg transition"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
